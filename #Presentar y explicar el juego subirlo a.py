76d56bc9aee4aaf2160d90a88011d3cef23b5c2e
andrenavd <165317049+andrenavd@users.noreply.github.com>
2024-03-29 07:31:10 -0600

    Add files via upload

---
+++ #Presentar y explicar el juego subirlo a.py
@@ -0,0 +1,5 @@
+#Presentar y explicar el juego subirlo a branches 
+# The game is 2048, its a game where you have to connect blocks of the same value in order to make a bigger block
+# The unit of the block multiplies, and every connection a new 2 unit square or a 4 unit square will appear on the board
+# you can only connect blocks of equal value and the goal is to get to a block of the value 2048
+# You lose when you can't connect anymore blocks.
