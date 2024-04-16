332732512e6ac336eb634724531e331e308281b0
andrenavd <165317049+andrenavd@users.noreply.github.com>
2024-04-15 21:54:43 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-print 
+print the board, show pieces
 def _(new_board):
     # Defining the board 
     checkers.board = [
@@ -15,3 +15,33 @@ def _(new_board):
 def print_board(checkers):
     for row in checkers.board:
         print(' '.join(row))
+ # PLayers
+checkers.player1 = 'X'  
+checkers.player2 = 'O'  
+checkers.current_player = checkers.player1  
+
+def print_board(checkers):
+    for row in checkers.board:
+        print(' '.join(row))
+
+def is_valid_move(checkers, start, end):
+    # See if that move can be done 
+    pass
+
+def make_move(checkers, start, end):
+    # How to get the movement made 
+    pass
+
+def switch_player(checkers):
+    # Switch Player
+    if checkers.current_player == checkers.player1:
+        checkers.current_player = checkers.player2
+    else:
+        checkers.current_player = checkers.player1
+
+def is_winner(checkers):
+       
+        
+
+    game = CheckersGame()
+game.print_board()
