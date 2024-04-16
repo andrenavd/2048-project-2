c6a0f42c377f44902fec8bc2229657dfdca6d503
andrenavd <165317049+andrenavd@users.noreply.github.com>
2024-04-16 05:24:36 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,49 +1,50 @@
-print the board
-print pieces and their colors 
-def _(new_board):
-    # Defining the board 
-    checkers.board = [
-        ['_', 'X', '_', 'X', '_', 'X', '_', 'X'],
-        ['X', '_', 'X', '_', 'X', '_', 'X', '_'],
-        ['_', 'X', '_', 'X', '_', 'X', '_', 'X'],
-        ['_', '_', '_', '_', '_', '_', '_', '_'],
-        ['_', '_', '_', '_', '_', '_', '_', '_'],
-        ['O', '_', 'O', '_', 'O', '_', 'O', '_'],
-        ['_', 'O', '_', 'O', '_', 'O', '_', 'O'],
-        ['O', '_', 'O', '_', 'O', '_', 'O', '_']
-    ]
-print player 1 pieces
-print player 2 pieces 
-def print_board(checkers):
-    for row in checkers.board:
-        print(' '.join(row))
- # PLayers
-checkers.player1 = 'X'  
-checkers.player2 = 'O'  
-checkers.current_player = checkers.player1  
+class CheckersGame:
+    def __init__(self):
+        self.board = [
+            [' ', 'X', ' ', 'X', ' ', 'X', ' ', 'X'],
+            ['X', ' ', 'X', ' ', 'X', ' ', 'X', ' '],
+            [' ', 'X', ' ', 'X', ' ', 'X', ' ', 'X'],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            ['O', ' ', 'O', ' ', 'O', ' ', 'O', ' '],
+            [' ', 'O', ' ', 'O', ' ', 'O', ' ', 'O'],
+            ['O', ' ', 'O', ' ', 'O', ' ', 'O', ' '],
+        ]
+        self.current_player = 'X'
 
-def print_board(checkers):
-    for row in checkers.board:
-        print(' '.join(row))
+    def print_board(self):
+        for row in self.board:
+            print(" ".join(row))
 
-def is_valid_move(checkers, start, end):
-    # See if that move can be done 
-    pass
+    def is_valid_move(self, start, end):
+        # Implement logic to check if the move is valid
+        pass
 
-def make_move(checkers, start, end):
-    # How to get the movement made 
-    pass
+    def make_move(self, start, end):
+        # Implement logic to make the move
+        pass
 
-def switch_player(checkers):
-    # Switch Player
-    if checkers.current_player == checkers.player1:
-        checkers.current_player = checkers.player2
-    else:
-        checkers.current_player = checkers.player1
+    def switch_player(self):
+        self.current_player = 'O' if self.current_player == 'X' else 'X'
 
-def is_winner(checkers):
-       
-        
+    def is_winner(self):
+        # Implement logic to check if there's a winner
+        pass
 
+    def play(self):
+        while not self.is_winner():
+            self.print_board()
+            print(f"\n{self.current_player}'s turn:")
+            start = tuple(map(int, input("Enter start position (row col): ").split()))
+            end = tuple(map(int, input("Enter end position (row col): ").split()))
+
+            if self.is_valid_move(start, end):
+                self.make_move(start, end)
+                self.switch_player()
+            else:
+                print("Invalid move, try again!")
+
+
+if __name__ == "__main__":
     game = CheckersGame()
-game.print_board()
+    game.play()
