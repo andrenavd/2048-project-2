a0560ad24f982bc3944ab0f5a1bc74e88997d174
rodrigogarciao <165348510+rodrigogarciao@users.noreply.github.com>
2024-04-16 08:51:59 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -16,35 +16,4 @@ class CheckersGame:
         for row in self.board:
             print(" ".join(row))
 
-    def is_valid_move(self, start, end):
-     
-        pass
-
-    def make_move(self, start, end):
-       
-        pass
-
-    def switch_player(self):
-        self.current_player = 'O' if self.current_player == 'X' else 'X'
-
-    def is_winner(self):
-       
-        pass
-
-    def play(self):
-        while not self.is_winner():
-            self.print_board()
-            print(f"\n{self.current_player}'s turn:")
-            start = tuple(map(int, input("Enter start position (row col): ").split()))
-            end = tuple(map(int, input("Enter end position (row col): ").split()))
-
-            if self.is_valid_move(start, end):
-                self.make_move(start, end)
-                self.switch_player()
-            else:
-                print("Invalid move, try again!")
-
-
-if __name__ == "__main__":
-    game = CheckersGame()
-    game.play()
+   
