f25731d614f30ab32b3a7ca0c11c3a6272475d02
rodrigogarciao <165348510+rodrigogarciao@users.noreply.github.com>
2024-04-17 17:25:59 -0400

    _main_.py

--- __main__.py
+++ __main__.py
@@ -1,8 +1,23 @@
-from board import create_new_board, print_board
-from actions import place
+from board import new_board, print_board
+from actions import switch_player, is_winner, is_valid_move, make_move
 
-with open('README.md', 'r') as f:
-    print(f.read())
+board = new_board()
+
+while not is_winner():
+    print_board(board)
+    
+    start = input(f"Player {current_player}, enter start position: ").upper()
+    end = input(f"Player {current_player}, enter end position: ").upper()
+    
+    if is_valid_move(start, end):
+        make_move(start, end)
+        switch_player()
+    else:
+        print("That can´t be done, try again.")
+
+print_board(board)
+print(f"Player {current_player} is so good, he just won by so much!")
+
 
 
    
