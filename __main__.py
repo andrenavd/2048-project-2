1868a7c1b4aa8de3182282d306d4e612b9c778d9
rodrigogarciao <165348510+rodrigogarciao@users.noreply.github.com>
2024-04-29 22:15:08 -0400

    __main__.py

--- __main__.py
+++ __main__.py
@@ -2,21 +2,25 @@ from board import new_board, print_board
 from actions import switch_player, is_winner, is_valid_move, make_move
 
 board = new_board()
+player1 = 'X'
+player2 = 'O'
+current_player = player1
 
-while not is_winner():
+while not is_winner(board, current_player):
     print_board(board)
     
     start = input(f"Player {current_player}, enter start position: ").upper()
     end = input(f"Player {current_player}, enter end position: ").upper()
     
-    if is_valid_move(start, end):
-        make_move(start, end)
+    if is_valid_move(board, start, end):
+        make_move(board, start, end, current_player)
         switch_player()
     else:
-        print("That can´t be done, try again.")
+        print("That can't be done, try again.")
 
 print_board(board)
 print(f"Player {current_player} is so good, he just won by so much!")
+
 
 
 
