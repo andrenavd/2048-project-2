aaec7ac7cd3ad16256c1d9dd63fd2e3ca9db417e
rodrigogarciao <165348510+rodrigogarciao@users.noreply.github.com>
2024-05-06 22:23:56 -0400

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,24 +1,46 @@
-player1 = 'X'
-player2 = 'O'
-current_player = player1
+def is_valid_move(board, start, end, player):
+    start_row, start_col = start
+    end_row, end_col = end
 
-def print_board(board):
-    for row in board:
-        print(' '.join(row))
+    if not (0 <= start_row < 8 and 0 <= start_col < 8):
+        return False
 
-def is_valid_move(board, start, end):
-    pass
+    if not (0 <= end_row < 8 and 0 <= end_col < 8):
+        return False
+
+    if board[end_row][end_col] != '_':
+        return False
+
+  
+    if player == 'X':
+        if end_row < start_row or abs(end_row - start_row) != 1 or abs(end_col - start_col) != 1:
+            return False
+    elif player == 'O':
+        if end_row > start_row or abs(end_row - start_row) != 1 or abs(end_col - start_col) != 1:
+            return False
+    
+    return True
 
 def make_move(board, start, end, player):
-    pass
+    start_row, start_col = start
+    end_row, end_col = end
 
-def switch_player():
-    global current_player
-    if current_player == player1:
-        current_player = player2
+
+    board[start_row][start_col] = '_'
+    board[end_row][end_col] = player
+
+def switch_player(current_player):
+    if current_player == 'X':
+        return 'O'
     else:
-        current_player = player1
+        return 'X'
 
 def is_winner(board, player):
-    pass
+    
+    for row in board:
+        for cell in row:
+            if cell == switch_player(player):
+                return False
+    return True
+
 
